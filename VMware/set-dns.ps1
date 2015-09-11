bb2bb689da4bd99ff12148f4cee09fce9c58cd35
Chris Wahl <github@wahlnetwork.com>
2015-09-10 22:05:05 -0500

    Update Set-DNS with PowerCLI version check
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- VMware/set-dns.ps1
+++ VMware/set-dns.ps1
@@ -7,8 +7,34 @@
 $vars = (Get-Item $PSScriptRoot).Parent.FullName + "\vars.ps1"
 Invoke-Expression ($vars)
 
-### Import modules
-Add-PSSnapin -Name VMware.VimAutomation.Core
+### Import modules or snapins
+$powercli = Get-PSSnapin -Name VMware.VimAutomation.Core -Registered
+
+try 
+{
+    switch ($powercli.Version.Major) {
+        {
+            $_ -ge 6
+        }
+        {
+            Import-Module -Name VMware.VimAutomation.Core -ErrorAction Stop
+            Write-Host -Object 'PowerCLI 6+ module imported'
+        }
+        5
+        {
+            Add-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction Stop
+            Write-Warning -Message 'PowerCLI 5 snapin added; recommend upgrading your PowerCLI version'
+        }
+        default 
+        {
+            throw 'This script requires PowerCLI version 5 or later'
+        }
+    }
+}
+catch 
+{
+    throw 'Could not load the required VMware.VimAutomation.Core cmdlets'
+}
 
     # Ignore self-signed SSL certificates for vCenter Server (optional)
     Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -DisplayDeprecationWarnings:$false -Scope User -Confirm:$false
