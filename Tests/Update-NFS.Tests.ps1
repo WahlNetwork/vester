9159b7583823d791ab74bef2a4564ecc9d20676d
Carlos Tronco <Carlos.Tronco@nike.com>
2016-08-23 15:28:06 -0700

    Modified NFS Test to speed up?

--- Tests/Update-NFS.Tests.ps1
+++ Tests/Update-NFS.Tests.ps1
@@ -14,22 +14,33 @@ Param(
 
 Process {
     # Tests
-    Describe -Name 'Host Configuration: NFS Advanced Configuration' -Tag @("host","storage","nfs") -Fixture {
+    Describe -Name 'Host Configuration: NFS Advanced Configuration  - CRT ' -Tag @("host","storage","nfs") -Fixture {
         # Variables
         . $Config
         [System.Collections.Hashtable]$nfsadvconfig = $config.nfsadvconfig
-
+        $compare = @()
+        $nfsadvconfig.Values | ForEach-Object -Process {
+            $compare += $_
+        }
         foreach ($server in (Get-VMHost -Name $config.scope.host).name) 
         {
+            $hostadvcfg = Get-AdvancedSetting -Entity $server
+            $hostadvsettings = @{}
+            foreach ($setting in $hostadvcfg) { 
+                $sname = $setting.name
+                $svalue = $setting.value
+                $hostadvsettings[$sname] = $svalue
+        }
+            $value =@()
+            $hostadv
+            foreach ($setting in $nfsadvconfig.Keys) {
+                if ($hostadvsettings.ContainsKey($setting)){
+                    $value += $hostadvsetting.$setting
+                } else { 
+                    #nop 
+                }
+            }
             It -name "$server NFS Settings" -test {
-                $value = @()
-                $nfsadvconfig.Keys | ForEach-Object -Process {
-                    $value += (Get-AdvancedSetting -Entity $server -Name $_).Value
-                }
-                $compare = @()
-                $nfsadvconfig.Values | ForEach-Object -Process {
-                    $compare += $_
-                }
                 try 
                 {
                     $value | Should Be $compare
