a0d6077f73ba76c6d4a2c774e19c4052a8ed1496
brianbunke <brianbunke+github@gmail.com>
2016-09-06 10:57:35 -0700

    Update module files

--- Vester.psm1
+++ Vester.psm1
@@ -1,46 +1,15 @@
-﻿#Requires -Modules Pester, VMware.VimAutomation.Core
+﻿# Gather all files
+$PublicFunctions  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$PrivateFunctions = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
 
-<#
-.SYNOPSIS
-PowerCLI and Pester team up against your vSphere environment.
-
-.DESCRIPTION
-TODO: Write a full comment-based help section
-#>
-function Invoke-Vester {
-    [CmdletBinding(SupportsShouldProcess = $true, 
-                   ConfirmImpact = 'Medium')]
-    param (
-        # Optionally fix all config drift that is discovered. Defaults to false (off)
-        [switch]$Remediate = $false,
-
-        # Optionally define a different config file to use. Defaults to Vester\Configs\Config.ps1
-        [string]$Config = '.\Configs\Config.ps1',
-
-        # Optionally run only tests that match the given tag(s)
-        [ValidateSet('cluster','host','network','nfs','storage','vcenter','vds','vm')]
-        # TODO: ^ That should be dynamic
-        [string[]]$Tag,
-
-        # Optionally exclude tests with the given tag(s)
-        [ValidateSet('cluster','host','network','nfs','storage','vcenter','vds','vm')]
-        # TODO: ^ That should be dynamic
-        [string[]]$ExcludeTag
-    )
-
-    BEGIN {
-        # Put the logic to check for active Connect-VIServer session here
-        # See Config.Tests.ps1
+# Dot source the functions
+ForEach ($File in @($PublicFunctions + $PrivateFunctions)) {
+    Try {
+        . $File.FullName
+    } Catch {
+        Write-Error -Message "Failed to import function $($File.FullName): $_"
     }
+}
 
-    PROCESS {
-        Invoke-Pester -Tag $Tag -ExcludeTag $ExcludeTag -Script @{
-            # TODO: Figure out what to do with the config test
-            Path = '.\Tests\'
-            Parameters = @{
-                Remediate  = $Remediate
-                Config     = $Config
-            }
-        } # Invoke-Pester
-    } # Process
-} # function
+# Export the public functions for module use
+Export-ModuleMember -Function $PublicFunctions.Basename
