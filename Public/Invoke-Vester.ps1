7bfb072bb5db7104c46f3abb429940def6840105
brianbunke <brianbunke+github@gmail.com>
2016-09-14 21:20:01 -0700

    Add awesome comment-based help

--- Public/Invoke-Vester.ps1
+++ Public/Invoke-Vester.ps1
@@ -3,26 +3,81 @@
 function Invoke-Vester {
     <#
     .SYNOPSIS
-    PowerCLI and Pester team up against your vSphere environment.
+    Test and fix configuration drift in your VMware vSphere environment.
 
     .DESCRIPTION
-    TODO: Write a full comment-based help section
+    Invoke-Vester will run each test it finds and report on discrepancies.
+    It compares actual values against the values you supply in a config file,
+    and can fix them immediately if you include the -Remediate parameter.
+
+    If you are not already connected to the vCenter server defined in the
+    config file, Invoke-Vester will prompt for credentials to connect to it.
+
+    Invoke-Vester then calls Pester to run each test file. The test files
+    leverage PowerCLI to gather values for comparison/remediation.
+
+    .EXAMPLE
+    Invoke-Vester -Verbose
+    Using the default config file at \Configs\Config.ps1, Vester will run
+    all *.Tests.ps1 files found inside of the current location, recursively.
+    Verbose output will be displayed on the screen.
+    It outputs a report of all passed and failed tests.
+
+    .EXAMPLE
+    Invoke-Vester -Script C:\Tests\ -Config C:\Tests\Config.ps1
+    Vester runs all *.Tests.ps1 files found underneath the C:\Tests directory,
+    and compares values to the config file in the same location.
+    It outputs a report of all passed and failed tests.
+
+    .EXAMPLE
+    Get-ChildItem -Path Z:\ -Filter *dns* -File -Recurse | Invoke-Vester
+    Use Get-ChildItem to get all files below Z:\ with 'dns' in the name.
+    Pipe those files into Invoke-Vester to run them as tests.
+
+    .EXAMPLE
+    Invoke-Vester -Script .\Tests\VM -Remediate -WhatIf
+    Run all test files in the .\Tests\VM path below the current location.
+    For all tests that fail against the values in \Configs\Config.ps1,
+    -Remediate attempts to immediately fix them to match your defined config.
+    -WhatIf prevents remediation, and instead reports what would have changed.
+
+    .EXAMPLE
+    Invoke-Vester -Config .\Config-Dev.ps1 -Remediate
+    Run all test files in the current location, and compare values to those
+    defined within the Config-Dev.ps1 file at the current location.
+    For all failed tests, -Remediate attempts to immediately fix them
+    to match the defined values.
+
+    .INPUTS
+    [System.Object]
+    Accepts piped input via property name "FullName" (designed for Get-Item/Get-ChildItem)
+
+    .NOTES
+    This command relies on the Pester and PowerCLI modules for testing.
+
+    "Get-Help about_Vester" for more information.
+
+    .LINK
+    https://github.com/WahlNetwork/Vester
     #>
-    # Passes -WhatIf through to other tests
     [CmdletBinding(SupportsShouldProcess = $true,
                    ConfirmImpact = 'Medium')]
+    # Passes -WhatIf through to other tests
     param (
         # Define the file/folder of test file(s) to call
+        # Defaults to the current location
         [Parameter(ValueFromPipelineByPropertyName = $true)]
-        # Aliasing FullName enables easy pipe from Get-ChildItem
         [Alias('Path','FullName')]
         [object[]]$Script = '.',
+        # Aliasing FullName enables easy pipe from Get-ChildItem
 
-        # Optionally define a different config file to use. Defaults to Vester\Configs\Config.ps1
+        # Optionally define a different config file to use
+        # Defaults to Vester\Configs\Config.ps1
         # Currently only supports one Config file at a time
         [object]$Config = "$(Split-Path -Parent $PSScriptRoot)\Configs\Config.ps1",
 
-        # Optionally fix all config drift that is discovered. Defaults to false (off)
+        # Optionally fix all config drift that is discovered
+        # Defaults to false (disabled)
         [switch]$Remediate = $false
     )
 
