7c3f574bcd9b8ef20234d3c6db39d2d0c95c47ad
brianbunke <brianbunke+github@gmail.com>
2016-09-16 09:36:20 -0700

    Rearrange directories
    Add Vester module subfolder, split tests into categories

---
+++ Vester/Tests/Host/Update-DNS.Tests.ps1
@@ -0,0 +1,73 @@
+﻿#requires -Modules Pester, VMware.VimAutomation.Core
+
+[CmdletBinding(SupportsShouldProcess = $true, 
+               ConfirmImpact = 'Medium')]
+Param(
+    # Optionally fix all config drift that is discovered. Defaults to false (off)
+    [switch]$Remediate = $false,
+
+    # Optionally define a different config file to use. Defaults to Vester\Configs\Config.ps1
+    [string]$Config = (Split-Path $PSScriptRoot) + '\Configs\Config.ps1'
+)
+
+Process {
+    # Tests
+    Describe -Name 'Host Configuration: DNS Server(s)' -Tag @("host")-Fixture {
+        # Variables
+        . $Config
+        [array]$esxdns = $cfg.host.esxdns
+        [array]$searchdomains = $cfg.host.searchdomains
+
+        foreach ($server in (Get-VMHost -Name $cfg.scope.host)) 
+        {
+            It -name "$($server.name) Host DNS Address" -test {
+                [array]$value = (Get-VMHostNetwork -VMHost $server).DnsAddress
+                try 
+                {
+                    Compare-Object -ReferenceObject $esxdns -DifferenceObject $value | Should Be $null
+                }
+                catch 
+                {
+                    if ($Remediate) 
+                    {
+                        Write-Warning -Message $_
+                        # TODO: Update ShouldProcess with useful info
+                        if ($PSCmdlet.ShouldProcess("Target", "Operation"))
+                        {
+                            Write-Warning -Message "Remediating $server"
+                            Get-VMHostNetwork -VMHost $server | Set-VMHostNetwork -DnsAddress $esxdns -ErrorAction Stop
+                        }
+                    }
+                    else 
+                    {
+                        throw $_
+                    }
+                }
+            }
+            It -name "$($server.name) Host DNS Search Domain" -test {
+                [array]$value = (Get-VMHostNetwork -VMHost $server).SearchDomain
+                try 
+                {
+                    Compare-Object -ReferenceObject $searchdomains -DifferenceObject $value | Should Be $null
+                }
+                catch 
+                {
+                    if ($fix) 
+                    {
+                        Write-Warning -Message $_
+                        # TODO: Update ShouldProcess with useful info
+                        if ($PSCmdlet.ShouldProcess("Target", "Operation"))
+                        {
+                            Write-Warning -Message "Remediating $server"
+                            Get-VMHostNetwork -VMHost $server | Set-VMHostNetwork -SearchDomain $searchdomains -ErrorAction Stop
+                        }
+                    }
+                    else 
+                    {
+                        throw $_
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Vester/Tests/Storage/Update-NFS.Tests.ps1
@@ -0,0 +1,67 @@
+﻿#requires -Modules Pester, VMware.VimAutomation.Core
+
+[CmdletBinding(SupportsShouldProcess = $true, 
+               ConfirmImpact = 'Medium')]
+Param(
+    # Optionally fix all config drift that is discovered. Defaults to false (off)
+    [switch]$Remediate = $false,
+
+    # Optionally define a different config file to use. Defaults to Vester\Configs\Config.ps1
+    [string]$Config = (Split-Path $PSScriptRoot) + '\Configs\Config.ps1'
+)
+
+Process {
+    # Tests
+    Describe -Name 'Host Configuration: NFS Advanced Configuration' -Tag @("host","storage","nfs") -Fixture {
+        # Variables
+        . $Config
+        [System.Collections.Hashtable]$nfsadvconfig = $cfg.nfsadvconfig
+        $compare = @()
+        $nfsadvconfig.Values | ForEach-Object -Process {
+            $compare += $_
+        }
+        foreach ($server in (Get-VMHost -Name $cfg.scope.host).name) 
+        {
+            $hostadvcfg = Get-AdvancedSetting -Entity $server
+            $hostadvsettings = @{}
+            foreach ($setting in $hostadvcfg) { 
+                $sname = $setting.name
+                $svalue = $setting.value
+                $hostadvsettings[$sname] = $svalue
+        }
+            $value =@() 
+            foreach ($setting in $nfsadvconfig.Keys) {
+                if ($hostadvsettings.ContainsKey($setting)){
+                    $value += $hostadvsettings.$setting
+                } else { 
+                    #nop 
+                }
+            }
+            It -name "$server NFS Settings" -test {
+                try 
+                {
+                    $value | Should Be $compare
+                }
+                catch 
+                {
+                    if ($Remediate) 
+                    {
+                        Write-Warning -Message $_
+                        # TODO: Update ShouldProcess with useful info
+                        if ($PSCmdlet.ShouldProcess("Target", "Operation"))
+                        {
+                            Write-Warning -Message "Remediating $server"                    
+                            $nfsadvconfig.Keys | ForEach-Object -Process {
+                                Get-AdvancedSetting -Entity $server -Name $_ | Set-AdvancedSetting -Value $nfsadvconfig.Item($_) -Confirm:$false -ErrorAction Stop
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        throw $_
+                    }
+                }
+            }
+        }
+    }
+}
