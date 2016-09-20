88357d4a9ad5108916d6aa20df90dcd1f5d269d8
Travis Van Holland <travis.vanholland@xpo.com>
2016-09-20 10:08:08 -0500

    Virtual Machine Boot Delay
    Tests a value in milliseconds for each virtual machine

---
+++ Tests/Update-VMBootDelay.Tests.ps1
@@ -0,0 +1,47 @@
+﻿#requires -Modules Pester, VMware.VimAutomation.Core
+
+[CmdletBinding()]
+Param(
+    # Optionally fix all config drift that is discovered. Defaults to false (off)
+    [switch]$Remediate = $false,
+
+    # Optionally define a different config file to use. Defaults to Vester\Configs\Config.ps1
+    [string]$Config = (Split-Path $PSScriptRoot) + '\Configs\Config.ps1'
+)
+
+Process {
+    # Tests
+    Describe -Name 'VM : Boot Delay' -Tag @("vm") -Fixture {
+        # Variables
+        . $Config
+        [int]$VMBootDelay = $config.vm.bootDelay
+
+            foreach ($VM in (Get-VM -Name $config.scope.vm))
+            {
+                [int]$value = ($VM | Get-View).Config.BootOptions.BootDelay
+                It -name "$($VM.name) VM Boot Delay " -test {
+                    try 
+                    {
+                        $value | Should be $VMBootDelay
+                    }
+                    catch 
+                    {
+                        if ($Remediate)
+                        {
+                            Write-Warning -Message $_
+                            Write-Warning -Message "Remediating $VM"
+                            $vmbo = New-Object VMware.Vim.VirtualMachineBootOptions
+	                        $vmbo.BootDelay = $bootDelay
+	                        $vmcs = New-Object VMware.Vim.VirtualMachineConfigSpec
+	                        $vmcs.BootOptions = $vmbo
+	                        $VM.ExtensionData.ReconfigVM($vmcs)
+                        }
+                        else 
+                        {
+                            throw $_
+                        }
+                    }
+                }
+            }
+        }
+    }
