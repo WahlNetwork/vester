3cc63d452235089f604963347b36b97d19356cf7
brianbunke <brianbunke+github@gmail.com>
2016-11-02 13:44:57 -0700

    Move to new Tests format

---
+++ Vester/Tests/Host/DNS-Address.ps1
@@ -0,0 +1,15 @@
+﻿# Test title, e.g. 'DNS Servers'
+$Title = 'DNS Servers'
+
+# The config entry stating the desired values
+$Desired = $cfg.host.esxdns
+
+# The command(s) to pull the actual value for comparison
+$Actual = {
+(Get-VMHostNetwork -VMHost $Object).DnsAddress
+}
+
+# The command(s) to match the environment to the config
+$Fix = {
+Get-VMHostNetwork -VMHost $Object | Set-VMHostNetwork -DnsAddress $Desired -ErrorAction Stop
+}
---
+++ Vester/Tests/Host/DNS-SearchDomain.ps1
@@ -0,0 +1,15 @@
+﻿# Test title, e.g. 'DNS Servers'
+$Title = 'DNS Search Domains'
+
+# The config entry stating the desired values
+$Desired = $cfg.host.searchdomains
+
+# The command(s) to pull the actual value for comparison
+$Actual = {
+    (Get-VMHostNetwork -VMHost $Object).SearchDomain
+}
+
+# The command(s) to match the environment to the config
+$Fix = {
+    Get-VMHostNetwork -VMHost $Object | Set-VMHostNetwork -SearchDomain $Desired -ErrorAction Stop
+}
