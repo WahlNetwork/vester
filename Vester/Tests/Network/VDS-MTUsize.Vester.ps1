7f36720a0d87ea85edf356d6c13b214c0aa89c3a
John McCarthy <midactsmystery@gmail.com>
2017-04-17 23:34:14 -0400

    Issue #105 - Ported Network: VDS-MTUsize (#127)

---
+++ Vester/Tests/Network/VDS-MTUsize.Vester.ps1
@@ -0,0 +1,26 @@
+﻿# Test file for the Vester module - https://github.com/WahlNetwork/Vester
+# Called via Invoke-Pester VesterTemplate.Tests.ps1
+
+# Test title, e.g. 'DNS Servers'
+$Title = 'VDS MTU Size'
+
+# Test description: How New-VesterConfig explains this value to the user
+$Description = 'Set the MTU size for the VDS'
+
+# The config entry stating the desired values
+$Desired = $cfg.vds.mtu
+
+# The test value's data type, to help with conversion: bool/string/int
+$Type = 'int'
+
+# The command(s) to pull the actual value for comparison
+# $Object will scope to the folder this test is in (Cluster, Host, etc.)
+[ScriptBlock]$Actual = {
+    $Object.MTU
+}
+
+# The command(s) to match the environment to the config
+# Use $Object to help filter, and $Desired to set the correct value
+[ScriptBlock]$Fix = {
+    Set-VDSwitch $Object -LinkDiscoveryProtocol $Desired -Confirm:$FALSE -ErrorAction Stop
+}
