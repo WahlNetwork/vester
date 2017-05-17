0a8b87807e60606fe3006a65bbb429958a122d34
Justin Sider <jsider@belaytech.com>
2017-05-16 23:19:47 -0400

    Issue #116 - Update to ensure $object.name is correct for vCenter (#129)
    Add `Name` property to `$InventoryList` object on `vCenter` switch

--- Vester/Private/Template/VesterTemplate.Tests.ps1
+++ Vester/Private/Template/VesterTemplate.Tests.ps1
@@ -34,7 +34,7 @@ foreach($Scope in $Scopes)
     # Use $Scope (parent folder) to get the correct objects to test against
     # If changing values here, update the "$Scope -notmatch" test below as well
     $InventoryList = switch ($Scope) {
-        'vCenter'    {$cfg.vcenter.vc}
+        'vCenter'    {$global:DefaultVIServer | where-object {$_.name -like "$($cfg.vcenter.vc)"}}
         'Datacenter' {$Datacenter}
         'Cluster'    {$Datacenter | Get-Cluster -Name $cfg.scope.cluster}
         'DSCluster'  {$Datacenter | Get-DatastoreCluster -Name $cfg.scope.dscluster}
