d095dcb0b127bfcb94c19ac25cf1cbfdd4786b26
brianbunke <me@brianbunke.com>
2017-07-11 15:02:54 -0700

    New private function VesterGCI

---
+++ Vester/Private/Get-VesterChildItem.ps1
@@ -0,0 +1,35 @@
+﻿# Modified Get-ChildItem for Vester purposes
+# Handles the different logic paths allowed by Get-VesterTest
+
+function Get-VesterChildItem {
+    [CmdletBinding()]
+    param (
+        $Path,
+        $Scope,
+        $Name
+    )
+
+    If ($Scope -and $Name) {
+        Get-ChildItem -Path $Path -Filter "$Name.Vester.ps1" -File -Recurse |
+            Select-Object @{n='Name';e={($_.BaseName -split '\.')[0]}},
+                          @{n='Scope';e={($_.Directory -split '\\')[-1]}},
+                          FullName |
+            Where-Object Scope -in $Scope
+    } ElseIf ($Scope) {
+        Get-ChildItem -Path $Path -Filter '*.Vester.ps1' -File -Recurse |
+            Select-Object @{n='Name';e={($_.BaseName -split '\.')[0]}},
+                          @{n='Scope';e={($_.Directory -split '\\')[-1]}},
+                          FullName |
+            Where-Object Scope -in $Scope
+    } ElseIf ($Name) {
+        Get-ChildItem -Path $Path -Filter "$Name.Vester.ps1" -File -Recurse |
+            Select-Object @{n='Name';e={($_.BaseName -split '\.')[0]}},
+                          @{n='Scope';e={($_.Directory -split '\\')[-1]}},
+                          FullName
+    } Else {
+        Get-ChildItem -Path $Path -Filter '*.Vester.ps1' -File -Recurse |
+            Select-Object @{n='Name';e={($_.BaseName -split '\.')[0]}},
+                          @{n='Scope';e={($_.Directory -split '\\')[-1]}},
+                          FullName
+    }
+}
