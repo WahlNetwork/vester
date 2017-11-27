675333abbe9d937aa633ed58803c43edcb404409
Doug Taliaferro <dougt42@gmail.com>
2017-11-27 15:43:35 -0500

    New cluster tests for EVC mode and Predictive DRS
    Adds cluster tests for EVC mode and Predictive DRS

---
+++ Vester/Tests/Cluster/DRS-PredictiveEnabled.Vester.ps1
@@ -0,0 +1,30 @@
+# Test file for the Vester module - https://github.com/WahlNetwork/Vester
+# Called via Invoke-Pester VesterTemplate.Tests.ps1
+
+# Test title, e.g. 'DNS Servers'
+$Title = 'Predictive DRS State'
+
+# Test description: How New-VesterConfig explains this value to the user
+$Description = 'On/off switch for Predictive Distributed Resource Scheduler (DRS) on the cluster'
+
+# The config entry stating the desired values
+$Desired = $cfg.cluster.predictivedrsenable
+
+# The test value's data type, to help with conversion: bool/string/int
+$Type = 'bool'
+
+# The command(s) to pull the actual value for comparison
+# $Object will scope to the folder this test is in (Cluster, Host, etc.)
+[ScriptBlock]$Actual = {
+    ($Object | Get-View).ConfigurationEx.ProactiveDrsConfig.Enabled
+}
+
+# The command(s) to match the environment to the config
+# Use $Object to help filter, and $Desired to set the correct value
+[ScriptBlock]$Fix = {
+    $clusterview = Get-Cluster -Name $Object | Get-View
+    $clusterspec = New-Object -TypeName VMware.Vim.ClusterConfigSpecEx
+    $clusterspec.proactiveDrsConfig = New-Object -TypeName VMware.Vim.ClusterProactiveDrsConfigInfo
+    $clusterspec.proactiveDrsConfig.enabled = $Desired
+    $clusterview.ReconfigureComputeResource_Task($clusterspec, $true)    
+}
