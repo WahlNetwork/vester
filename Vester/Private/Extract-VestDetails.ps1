89e78c60bbaee1ceb1dbb18d4a4cb63da77bf277
Carlos Tronco <cars@lostroncos.org>
2017-12-21 12:10:25 -0800

    Mod to Extract-VestDetails.ps1 for \ as a scriptblock
    Signed-off-by: Carlos Tronco <cars@lostroncos.org>

--- Vester/Private/Extract-VestDetails.ps1
+++ Vester/Private/Extract-VestDetails.ps1
@@ -10,11 +10,15 @@ function Extract-VestDetails {
 
     # Create $Title/$Description/$Type/$Actual/$Fix from the test file
     . $Object.FullName
-                        
-    # That doesn't work to find $Desired's value, so do it here
-    $Desired = ((Select-String -Path $Object.FullName -Pattern 'Desired \=').Line -split ' ')[-1]
-
-    # Output all properties for capturing in a $Vest variable
+    
+    #account for $Desired possibly being a script block. If it is don't do anything special (treat like $Actual)
+    # if not get the value as before
+    if ( -Not (Select-String -Path $Object.FullName -Pattern '\$Desired \=').Line.ToLower().Contains('scriptblock') ){
+        # That doesn't work to find $Desired's value, so do it here
+        $Desired = ((Select-String -Path $Object.FullName -Pattern '\$Desired \=').Line -split ' ')[-1]
+   }
+ 
+   # Output all properties for capturing in a $Vest variable
     [PSCustomObject]@{
         # Add a custom type name for this object
         # Used with DefaultDisplayPropertySet
